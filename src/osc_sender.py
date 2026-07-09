614293b9a5b2356df875ed6c015660473e745b61
James Jacob <jakwob@gmail.com>
2026-07-09 13:35:17 +0100

    add pillar and OSC configuration

--- src/osc_sender.py
+++ src/osc_sender.py
@@ -1,11 +1,14 @@
 from pythonosc.udp_client import SimpleUDPClient
 
+from src.config import PILLAR_ID, OSC_IP, OSC_PORT
+
 
 class OSCSender:
-    def __init__(self, ip="127.0.0.1", port=8000):
-        self.client = SimpleUDPClient(ip, port)
+    def __init__(self):
+        self.client = SimpleUDPClient(OSC_IP, OSC_PORT)
 
     def send_pillar(self, presence, distance, activity):
-        self.client.send_message("/pillar/1/presence", float(presence))
-        self.client.send_message("/pillar/1/distance", float(distance))
-        self.client.send_message("/pillar/1/activity", float(activity))
+        self.client.send_message(f"/pillar/{PILLAR_ID}/presence", float(presence))
+        self.client.send_message(f"/pillar/{PILLAR_ID}/distance", float(distance))
+        self.client.send_message(f"/pillar/{PILLAR_ID}/activity", float(activity))
+        
